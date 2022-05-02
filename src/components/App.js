34d7047a0b77bc6b875731a4eef83c66effc0037
Shantel <snchristian8@gmail.com>
2022-05-02 18:28:29 -0400

    setup fetch for candies

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react"
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home"
+
+import CandyList from './CandyList';
+import Navbar from "./Navbar";
 
 function App() {
+  const [candies,setCandies] = useState ([])
+
+  useEffect ( ()=>{
+fetch("http://localhost:9292/candies")
+.then ((res) => res.json())
+.then ((data) => setCandies(data))
+},[])
+
+  
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navbar toggleCartOpen={toggleCartOpen} />
+      <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/candies" element={<CandyList candies={candies}/>}/>
+     </Routes>
+    </div>  
   );
 }
 
